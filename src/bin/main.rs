a5ad7edb28b7164e89398aeac45423522d3e956e
Jesse Cranney <jesse.cranney@anu.edu.au>
2026-06-07 22:21:41 +1000

    ok, running actually now

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,22 +1,22 @@
 use risio::{Image, ImageType, ValidImage};
 
 fn main() {
-    let mut image = Image::<f64>::read_sharedmem_image("shmim_from_rust", &[10, 10]).unwrap();
-    // image.array()[0] += 0.1;
-    image.array()[1] += 0.1;
+    // let mut image = Image::<f64>::read_sharedmem_image("shmim_from_rust", &[10, 10]).unwrap();
+    // // image.array()[0] += 0.1;
+    // image.array()[1] += 0.1;
     // println!("{:?}", image.array());
     // println!("hello risio!");
 
-    // let mut image = Image::<f64>::create_image(
-    //     "shmim_from_rust",
-    //     2,
-    //     &[10, 10],
-    //     -1,
-    //     true,
-    //     5,
-    //     10,
-    //     ImageType::default(),
-    //     1,
-    // )
-    // .unwrap();
+    let mut image = Image::<f64>::create_image(
+        "shmim_from_rust",
+        2,
+        &[10, 10],
+        -1,
+        true,
+        0,
+        10,
+        ImageType::default(),
+        1,
+    )
+    .unwrap();
 }

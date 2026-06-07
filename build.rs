2bad1cd60180eee9242365372e7d7e717ddcd73c
Jesse Cranney <jesse.cranney@anu.edu.au>
2026-06-07 21:49:53 +1000

    alright, reading and writing to shm through rust with isio format

--- build.rs
+++ build.rs
@@ -2,12 +2,17 @@ fn main() {
     let build_path = cmake::build("./libmilk");
     
     // Use the following commands to link to shared libraries
-    println!("cargo:rustc-link-search={}/lib", build_path.display()); // directory
-    println!("cargo:rustc-link-lib=static=milk");
+    println!("cargo:rustc-link-search={}/milk-1.03.00/lib", build_path.display()); // directory
+    println!("cargo:rustc-link-lib=static=ImageStreamIO");
+    println!("cargo:rustc-link-lib=static=milkCOREMODarith");
+    println!("cargo:rustc-link-lib=static=milkCOREMODiofits");
+    println!("cargo:rustc-link-lib=static=milkCOREMODmemory");
+    println!("cargo:rustc-link-lib=static=milkCOREMODtools");
     
     // Generate bindings with bindgen
     let bindings = bindgen::Builder::default()
-        .header("libmilk/src/CommandLineInterface/IMGID.h")
+        .clang_arg("-Ilibmilk/src")
+        .header("./libmilk/src/CommandLineInterface/CLIcore.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Unable to generate bindings");
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,14 +1,13 @@
-// use risio::{Image, ImageType, ValidImage};
+use risio::{Image, ImageType, ValidImage};
 
 fn main() {
-    // let mut image = Image::<u8>::read_sharedmem_image("shmim_from_rust", &[10, 10]).unwrap();
-    // for _ in 0..100000 {
-    //     let mut image = Image::<u8>::read_sharedmem_image("shmim_from_rust", &[10, 10]).unwrap();
-    //     image.array()[0] += 1;
-    // }
+    let mut image = Image::<f64>::read_sharedmem_image("shmim_from_rust", &[10, 10]).unwrap();
+    // image.array()[0] += 0.1;
+    image.array()[1] += 0.1;
     // println!("{:?}", image.array());
+    // println!("hello risio!");
 
-    // let mut image = Image::<u8>::create_image(
+    // let mut image = Image::<f64>::create_image(
     //     "shmim_from_rust",
     //     2,
     //     &[10, 10],
@@ -20,11 +19,4 @@ fn main() {
     //     1,
     // )
     // .unwrap();
-    // println!("{:?}", image.array.);
-    // println!("{:?}", image);
-    // println!("{:?}", image.array());
-    // let array = image.array();
-    // array[0] = 10;
-    // println!("{:?}", image.array());
-    // println!("hello risio!");
 }
